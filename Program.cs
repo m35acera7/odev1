61241c3b1cbc2fb100f3f61050140dc346916f65
m35acera7 <124085627+m35acera7@users.noreply.github.com>
2023-01-31 22:42:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tekSayi = 13;
+            int çiftSayi = 24;
+
+
+            Console.WriteLine(tekSayi + çiftSayi);
+            Console.ReadLine();
+
+
+        
+    }
+}
+        }
